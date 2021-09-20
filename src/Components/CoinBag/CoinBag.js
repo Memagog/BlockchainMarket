7f60dd3c8e09833aba73fa1806173080e75823dc
Memagog <55204188+Memagog@users.noreply.github.com>
2021-09-20 14:52:41 +0300

    created CoinBag component add styles and modal view

--- src/Components/CoinBag/CoinBag.js
+++ src/Components/CoinBag/CoinBag.js
@@ -1,10 +1,67 @@
 import React from 'react';
 import './CoinBag.scss';
+import { FiMinusSquare } from 'react-icons/fi';
 
-export default function CoinBag() {
+const myBag = [
+    {
+        name: "BTC",
+        price: 46000,
+        amount: 1.200,
+        percentage: "2.3%"
+    },
+    {
+        name: "ETH",
+        price: 4000,
+        amount: 1.200,
+        percentage: "5.3%"
+    },
+    {
+        name: "DOG",
+        price: 1000,
+        amount: 1.200,
+        percentage: "4.3%"
+    },
+    {
+        name: "CHIA",
+        price: 1200,
+        amount: 1.200,
+        percentage: "12.3%"
+    },
+    {
+        name: "XHR",
+        price: 0.333,
+        amount: 2200,
+        percentage: "45.3%"
+    }
+]
+export default function CoinBag() {  
     return (
-        <div>
-            CoinBag
-        </div>
-    )
+       <div>
+         <table class="table table-dark table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">№</th>
+                        <th scope="col">Coin</th>
+                        <th scope="col">Price</th>
+                        <th scope="col">Amount</th>
+                        <th scope="col">%</th>
+                        <th scope="col">Del</th>
+                    </tr>
+                </thead>
+                <tbody>                      
+                    {myBag.map((el,i)=>
+                        <tr>
+                            <th scope="row">{i+1}</th>
+                            <td>{el.name}</td>
+                            <td>{el.price}</td>
+                            <td>{el.amount}</td>    
+                            <td style={{textAlign:"left"}}>{el.percentage}</td>
+                            <td><FiMinusSquare className="button-minus"/></td>  
+                        </tr>
+                         
+                    )}              
+                </tbody>
+            </table>
+       </div>
+    );
 }
--- src/Components/Header/HeaderBag.js
+++ src/Components/Header/HeaderBag.js
@@ -1,13 +1,40 @@
-import React from 'react'
-import { useHistory } from "react-router-dom";
+import React,{useState} from 'react'
 import { GiReceiveMoney } from 'react-icons/gi';
+import { Button, Modal } from 'react-bootstrap';
+import CoinBag from '../CoinBag/CoinBag';
+
 export default function HeaderBag() {
-    const history = useHistory();
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
     return (
-        <div className="header__bag-container" onClick={()=>history.push("/bag")}>
-            <GiReceiveMoney/>
-            <span>135 USD</span>
-            <span>+100%</span>
+        <div>
+           
+            <div className="header__bag-container" onClick={handleShow}>
+                <GiReceiveMoney/>
+                <span>135 USD</span>
+                <span>+100%</span>
+            </div>
+            <>
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                    <Modal.Title>Modal heading</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <CoinBag></CoinBag>
+                    </Modal.Body>
+                    <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        Save Changes
+                    </Button>
+                    </Modal.Footer>
+                </Modal>
+            </>
         </div>
+       
     )
 }
