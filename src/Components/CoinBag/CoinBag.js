7ed7861a1e6ecc62321296b4e08a11d2310e9600
Memagog <55204188+Memagog@users.noreply.github.com>
2021-09-20 12:05:37 +0300

    created the initial structure for UI

---
+++ src/Components/CoinBag/CoinBag.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './CoinBag.scss';
+
+export default function CoinBag() {
+    return (
+        <div>
+            CoinBag
+        </div>
+    )
+}
---
+++ src/Components/CoinInfo/Coin.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Coin.scss'
+
+export default function Coin() {
+    return (
+        <div>
+            Coin
+        </div>
+    )
+}
