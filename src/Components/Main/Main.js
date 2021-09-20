bd8298d856fb29ea6c2aa9a7bb57beccd027d41e
Memagog <55204188+Memagog@users.noreply.github.com>
2021-09-20 12:34:04 +0300

    create Main-page add table-element, Header

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import './Main.scss'
+import Header from './../Header/Header';
+import MainTable from './Table/MainTable';
 
 export default function Main() {
     return (
-        <div>
-             MainPage
+        <div className="container">
+            <div className="header">
+                <Header/>
+            </div>
+            <div className="table">
+                <MainTable/>
+            </div>
         </div>
     )
 }
